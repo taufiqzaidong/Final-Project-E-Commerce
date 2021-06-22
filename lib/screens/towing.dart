ced13d288e870ece950587e6e048d9026a59ddca
MUHAMMAD FAIZ BIN ANUAR 198921 <198921@student.upm.edu.my>
2021-06-23 01:49:47 +0800

    towing

--- lib/screens/towing.dart
+++ lib/screens/towing.dart
@@ -1,9 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:gowallpaper/screens/home.dart';
+import 'package:gowallpaper/screens/Service.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:provider/provider.dart';
 
-class Towing extends StatelessWidget {
+class Towing extends StatefulWidget {
+  @override
+  _TowingState createState() => _TowingState();
+}
 
-@override
+class _TowingState extends State<Towing> {
+  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+
+  Position _currentPosition;
+  String _currentAddress;
+
+  @override
   Widget build(BuildContext context) {
-    
+    return Scaffold(
+      backgroundColor: Color(0xFFCBC3E3),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF5C0B68),
+        elevation: 0,
+        centerTitle: true,
+        title: Text("Towing Service",
+            style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFFFFD119)),
+            textAlign: TextAlign.center),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Text("",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF212121)),
+                textAlign: TextAlign.center),
+            Image.asset(
+              "assets/towing.gif",
+              fit: BoxFit.cover,
+              width: double.infinity,
+            ),
+            SizedBox(
+              height: 35,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 100,
+            ),
+            ButtonTheme(
+              minWidth: MediaQuery.of(context).size.width / 1.1,
+              height: MediaQuery.of(context).size.height / 13,
+              child: RaisedButton.icon(
+                icon: Icon(Icons.location_city),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                color: Colors.purple[400],
+                textColor: Colors.white,
+                label: Text('Get Your Current Location'),
+                onPressed: () {
+                  _getCurrentLocation();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _getCurrentLocation() {
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+
+      _getAddressFromLatLng();
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  _getAddressFromLatLng() async {
+    try {
+      List<Placemark> p = await geolocator.placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
+
+      Placemark place = p[0];
+
+      setState(() {
+        _currentAddress =
+            "${place.locality}, ${place.postalCode}, ${place.country}";
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 }
